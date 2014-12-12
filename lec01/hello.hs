51219f70af456d9332b1dba76ac3b553e1d04b40
Jason <bjason@gmail.com>
2014-12-12 01:05:30 -0800

    more basic fxns

--- lec01/hello.hs
+++ lec01/hello.hs
@@ -6,3 +6,19 @@ string2 =  "world"
 greeting = string1 ++ " " ++ string2
 
 
+
+rr = sqrt 3
+
+
+
+square x = x * x
+
+
+
+posOrNeg x = 
+
+    if x >= 0
+        then "Positive"
+        else "Negative"
+
+
