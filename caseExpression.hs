4f27320072e1351786267d0a4df02e281567a7d2
Jason <bjason@gmail.com>
2014-12-13 19:13:20 -0800

    remove odds

--- caseExpression.hs
+++ caseExpression.hs
@@ -1,6 +1,20 @@
 
+-- Simple case expression
 double nums =  
     case nums of
         [] -> []
         (x :xs) -> (2 * x) : (double xs)
+
+
+
+removeOdd nums =
+    case nums of
+        [] -> []
+
+
+        (x : xs) -> 
+            case (mod x 2) of
+                0 ->   x :  (removeOdd xs) -- keep the even number
+                othewise ->  removeOdd xs  
+
 
