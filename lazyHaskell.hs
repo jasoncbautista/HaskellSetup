8081cb8920bb9083a07b57522bb6236e47992777
Jason <bjason@gmail.com>
2014-12-13 23:33:59 -0800

    not null

--- lazyHaskell.hs
+++ lazyHaskell.hs
@@ -17,7 +17,29 @@ pass x f  = f x
 
 pass3 = pass 3
 
-
 add1 n = n + 1
+
+
+-- operators
+
+(a,b) .+ (c, d) = (a + c , b + d)
+
+
+-- map
+loo = map length ["hello", "abc", "12"]
+
+oneUp = map (1+) [1..10]
+
+
+double = map (2*)
+
+
+
+notNull xs = not (null xs)
+cleanedList = filter notNull ["", "abc", "", "hello", ""]
+
+
+
+
 
 
