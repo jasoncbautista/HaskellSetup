06ac41ce3edb077cd4fcd74c12e9bd9cffcca19e
Jason <bjason@gmail.com>
2014-12-13 19:07:10 -0800

    remove odds pattern matched

--- patternMatching.hs
+++ patternMatching.hs
@@ -18,4 +18,16 @@ double [] = []
 double (x: xs) = (2*x) : (double xs)
 
 
+-- Guards
+
+removeOdd [] = []
+removeOdd (x: xs)
+    | mod x 2 == 0  = x : (removeOdd xs)
+    | otherwise    = removeOdd xs 
+
+
+
+
+
+
 
