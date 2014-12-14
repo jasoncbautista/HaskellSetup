1d2b43d84cee34b58a29c91a8642476a4f37370d
Jason <bjason@gmail.com>
2014-12-13 19:46:40 -0800

    infinite lists

--- caseExpression.hs
+++ caseExpression.hs
@@ -27,7 +27,7 @@ anyEven nums =
         (x : xs) -> True
 
 
-
+-- let binding
 fancySeven = 
     let a = 3
     in 2 * a + 1
@@ -37,5 +37,33 @@ fancyNine =
     let x = 4
         y = 5
     in x + y
+
+
+numEven nums = 
+    let evenNums = removeOdd nums
+    in lenth evenNums
+
+
+
+fancyTen = 
+    2 * (let a = 4 in 1 + a  )
+-- where binding
+
+fancySevenW = 2 * a + 1
+    where a = 3
+
+fancyNineW =  a + b
+    where a = 4
+        b = 5
+
+
+
+
+pairMax p = max  (fst p)
+                (snd p)
+    
+
+
+
 
 
---
+++ lazyHaskell.hs
@@ -0,0 +1,3 @@
+intsFrom n = n: (intsFrom (n+1))
+
+ints = intsFrom 1 
