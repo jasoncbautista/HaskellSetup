40e80a8b60a59f56a0889df54cbb714f559940b4
Jason <bjason@gmail.com>
2014-12-19 07:11:45 -0800

    basic parameterized types

---
+++ parametrerizedTypes.hs
@@ -0,0 +1,28 @@
+
+
+data Maybex a = Justt a | Nothingx
+
+
+
+x :: Maybex Int
+x = Nothingx
+
+
+
+y :: Maybex Int
+y = Justt 10
+
+fromMaybe :: a -> Maybex a -> a
+fromMaybe defaultValue Nothingx = defaultValue
+fromMaybe _ (Justt x) = x 
+
+
+
+yy = fromMaybe 0 y
+xx = fromMaybe 100 x
+
+
+
+
+
+
