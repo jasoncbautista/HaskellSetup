9e1e9f94bd9c85c6049e00b5234bbc5dbd70ab39
Jason <bjason@gmail.com>
2014-12-19 07:25:49 -0800

    get head of list

--- parametrerizedTypes.hs
+++ parametrerizedTypes.hs
@@ -23,6 +23,29 @@ xx = fromMaybe 100 x
 
 
 
+-- Simple List
+
+data Listx a = Emptyx | Cons a (Listx a)
+
+
+getHeadOfList ::  Listx a -> a
+getHeadOfList (Cons k _)   =  k
+
+
+ab = Emptyx
+cd = Cons 10 ab
+
+headOfCd = getHeadOfList cd
+
+
+
+-- TODO: use ^ 
+
+
+
+
+
+
 
 
 
